316a0ac9d06a4866020d49ab3e1ec1e34485e2a0
horyu <horyu@users.noreply.github.com>
2019-07-06 16:48:52 +0900

    use event.key instead of event.keyCode

--- e-typing-Shortcuts.user.js
+++ e-typing-Shortcuts.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         e-typing Shortcuts
 // @namespace    https://github.com/horyu/
-// @version      0.1
+// @version      0.2
 // @description  iframeに自動フォーカス ＆ ボタンショートカットの追加（Ctrl+Enter:スタート・もう一回、Alt+Enter:ミスだけ）
 // @match        https://www.e-typing.ne.jp/*
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
-(() => {
+(function () {
     'use strict';
     const observer = new MutationObserver(mutationObserverCallback);
     observer.observe(document.body, { childList: true, subtree: true });
@@ -17,7 +17,7 @@
         try {
             // continue break するためにforEachではなくfor文を使用
             for (let mutation of mutations) {
-                let iframe = [...mutation.addedNodes].find(node => node.tagName == 'IFRAME');
+                let iframe = [...mutation.addedNodes].find(node => node.tagName === 'IFRAME');
                 if (!iframe) {
                     continue;
                 }
@@ -34,11 +34,10 @@
 
     function addShortcuts(doc) {
         doc.addEventListener('keydown', (event) => {
-            // event.keyCode==13(Enter)
-            if (event.ctrlKey && event.keyCode==13) {
+            if (event.ctrlKey && event.key === 'Enter') {
                 clickById('start_btn');
                 clickById('replay_btn');
-            } else if (event.altKey && event.keyCode==13) {
+            } else if (event.altKey && event.key === 'Enter') {
                 clickById('miss_only_btn');
             }
         });
