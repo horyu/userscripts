56b852df45d19676b0196c6d271883d7e8f8f9f5
horyu <28817604+horyu@users.noreply.github.com>
2019-10-19 15:47:19 +0900

    Update TwitterImageViewer.user.js

--- TwitterImageViewer.user.js
+++ TwitterImageViewer.user.js
@@ -1,54 +1,53 @@
 // ==UserScript==
 // @name        Twitter Image Viewer
 // @namespace   https://github.com/horyu
-// @description ViewerjsをTwitterで使えるようにします。左側のメインメニューに追加された「View」ボタンで現在のタイムラインから取得できた画像をで開きます。デフォルトでは「Twitter Image Asist for React version」が必要となります、
+// @description 左側のメインメニューに追加された「View」ボタンで現在のタイムラインから取得できた画像をビューアーのように開きます。[画像の右側クリック/右キー入力]で次の画像、[画像の左側クリック/左キー入力]で前の画像、Escでビューアーを終了します。デフォルトでは「Twitter Image Asist for React version」が必要となります、
 // @include     https://twitter.com/*
-// @version     0.0.5
+// @version     0.1.0
 // @run-at      document-end
 // @noframes
-// @grant       GM_getResourceText
-// @require     https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.3.7/viewer.min.js
-// @resource    ViewerjsCSS https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.3.7/viewer.min.css
+// @require     https://gist.githubusercontent.com/horyu/148a014c447b4a9fbedad1b85e5be77f/raw/82bf75a13c191cf2698332f119c7f8485622dde4/wheelzoom.js
 // ==/UserScript==
 
 'use strict';
 
-// https://fengyuanchen.github.io/viewerjs/
-// https://github.com/fengyuanchen/viewerjs#options
-const viewerOptions = {
-    backdrop: true,
-    button: false,
-    navbar: true,
-    title: false,
-    toolbar: true,
-    tooltip: false,
-    movable: true,
-    zoomable: true,
-    rotatable: true,
-    scalable: true,
-    transition: false,
-    fullscreen: true,
-    keyboard: true,
-    backdrop: true,
-    loop: true,
-    loading: true,
-};
-
-function init() {
-    document.addEventListener('DOMContentLoaded', () => {
-        addStyle();
-        addViewButton();
-    });
-}
+document.addEventListener('DOMContentLoaded', () => {
+    addStyle();
+    addViewButton();
+});
 
 //
 // addStyle
 //
 
+const cssPrefix = 'horyususerscript-tiv';
+
 function addStyle() {
-    const styleEle = document.createElement('style');
-    styleEle.textContent = GM_getResourceText('ViewerjsCSS');;
-    document.head.appendChild(styleEle);
+    const style = document.createElement('style');
+    style.textContent = `
+.${cssPrefix}-root {
+    z-index: 9999;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+.${cssPrefix}-img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    max-width: calc(100% - 10px);
+    max-height: calc(100% - 10px);
+    object-fit: contain;
+    -moz-user-select: none;
+    user-select: none;
+}
+`;
+    document.head.appendChild(style);
 }
 
 //
@@ -56,41 +55,87 @@ function addStyle() {
 //
 
 function addViewButton() {
+    const ov = new OreViewer();
     const btn = document.createElement('button');
     btn.innerText = 'View';
     btn.onclick = () => {
-        setViewer(true);
+        ov.start(getImgURLs(true));
     }
     btn.oncontextmenu = (e) => {
         e.preventDefault();
-        setViewer(false);
+        ov.start(getImgURLs(false));
         return false;
     }
-    const id = setInterval(() => {
+    const intervalID = setInterval(() => {
         const ele = document.querySelector('[aria-label="メインメニュー"]');
         if (ele) {
             ele.appendChild(btn);
-            clearInterval(id);
+            clearInterval(intervalID);
         }
     }, 1000);
 };
 
-/* global Viewer */
-function setViewer(specificAccount) {
-    const galleryDiv = document.createElement('div');
-    galleryDiv.style.display = 'none';
-    const imgURLs = getImgURLs(specificAccount);
-    imgURLs.forEach(url => {
-        const img = document.createElement('img');
-        img.src = url;
-        galleryDiv.appendChild(img);
-    });
-    document.body.appendChild(galleryDiv);
-    const gallery = new Viewer(galleryDiv, viewerOptions);
-    gallery.view(0);
-    galleryDiv.addEventListener('hidden', () => {
-        galleryDiv.remove();
-    });
+class OreViewer {
+    constructor() {
+        this.root = document.createElement('div');
+        this.root.className = `${cssPrefix}-root`;
+        this.root.style.display = 'none';
+        let isSimpleClick = true;
+        this.root.onmousedown = () => {
+            isSimpleClick = true;
+        };
+        this.root.onmousemove = () => {
+            isSimpleClick = false;
+        };
+        this.root.onmouseup = (e) => {
+            if (isSimpleClick){
+                // クリックが画面の右側なら +1 左側なら -1
+                const diff = (e.clientX > this.root.clientWidth/2 ? 1 : -1);
+                this.addIndex(diff);
+            }
+        };
+        document.addEventListener('keydown', (e) => {
+            if (this.root.style.display === 'none') return;
+            switch (e.key) {
+                case 'ArrowLeft':
+                    this.addIndex(-1);
+                    break;
+                case 'ArrowRight':
+                    this.addIndex(1);
+                    break;
+                case 'Escape':
+                    this.root.style.display = 'none';
+                    break;
+            }
+        });
+        document.body.appendChild(this.root);
+    }
+    start(urls) {
+        if (urls.length === 0) return;
+        this.index = 0;
+        this.urls = urls;
+        const img = document.createElement('img'); // preload
+        for (const url of urls) img.src = url; // preload
+        this.root.style.display = '';
+        this.setImg();
+    }
+    addIndex(diff) {
+        this.index += diff;
+        if ((this.index < 0) || (this.index === this.urls.length)) {
+            this.root.style.display = 'none';
+        } else {
+            this.setImg();
+        }
+    }
+    setImg() {
+        const oldChild = this.root.firstChild;
+        const newChild = document.createElement('img');
+        newChild.src = this.urls[this.index];
+        newChild.className = `${cssPrefix}-img`;
+        this.root.appendChild(newChild);
+        if (oldChild) oldChild.remove();
+        window.wheelzoom(newChild);
+    }
 }
 
 function getImgURLs(specificAccount) {
@@ -125,11 +170,6 @@ function getImgURLs(specificAccount) {
             .sort((x, y) => x.style.order > y.style.order)
             .forEach(a => imgURLs.push(a.getAttribute('href')));
     });
+    console.log(imgURLs);
     return imgURLs;
 }
-
-//
-// init()
-//
-
-init();
